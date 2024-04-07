d6b126f4ea818b232f9a3363ad499b393f530fa8
KakveyKet <kakveyket408@gmail.com>
2024-04-07 17:29:35 +0700

    full auth

---
+++ resources/views/product/display.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>product this play page</title>
+</head>
+
+<body>
+    <h2>This is product</h2>
+</body>
+
+</html>
